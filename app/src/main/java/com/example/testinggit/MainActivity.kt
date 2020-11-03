f76e9a2a0c1e8ef9a39bbddb5aabdad61feb7282
Alpha-sk <alpha_sk@antistarforce.com>
2020-11-03 09:34:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/testinggit/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.testinggit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.firstActivity)
+    }
+}
